ec5c658904baddcfe693b30e1d025613c6b7c848
NathanCheshire <nathan.vincent.2.718@gmail.com>
2021-04-20 11:57:40 -0500

    redo code: maybe working algorithm but not functional yet

--- GOO.py
+++ GOO.py
@@ -1,17 +1,14 @@
 import math
 import random
 import time
-
-#list of lists of cycle lengths
-cycles = []
-currentN = 0
-
-#timeout for each goo
-msPerGoo = 1000
+import functools
+import collections
 
 #the goos you want to compute, you can have multiple
 goos = [12,40]
 
+#max order of symmetric group S12 is 60 using: [3, 4, 5]
+#max order of symmetric group S40 is 7140 using: [1, 2, 3, 4, 5, 7, 17]
 
 #note: computers are VERY fast, even a brute force algorithm like this doesn't take long to
 #      find the max order for a group
@@ -21,57 +18,60 @@ goos = [12,40]
 def main(): 
     for goo in goos:
         parts = GOO(goo)
-        print("Max order of symmetric group S", goo, " is ", int(parts[0]), " using ", parts[1], sep ='')
+        #print("Max order of symmetric group S", goo, " is ", int(parts[0]), " using ", parts[1], sep ='')
 
 #driver code, copy this if you're stealing my code for your own application :P
 # pls credit me though <3
 def GOO(n):
     max = 0
     using = []
-    cycles = []
+    globalCycles = []
     productOfCycleLengths = []
 
-    ones = []
-    for i in range(0, n):
-        ones.append(1)
-    
-    ones.sort()
-    cycles.append(ones)
+    #add to global cycles using procedural algorithm if generated sorted cycle is not in globalCycles
 
-    nList = []
-    nList.append(n)
-    nList.sort()
-    cycles.append(nList)
+    for i in range(1, n, 1):
+        currentCycle = [i]
+        for i in range(0, n - i, 1):
+            currentCycle.append(1)
 
-    i = n - 1
-    while i > 0:
-        cycle = []
-        cycle.append(i)
+        globalCycles.append(currentCycle)
+        print("here")
 
-        j = 0
-        while j < n - i:
-            cycle.append(1)
-            j += 1
+        if len(currentCycle) > 3:
+            firstCopy = currentCycle.copy()
+            sum = firstCopy[0] + firstCopy[1]
+            firstCopy[0] = sum
+            del firstCopy[1]
+            globalCycles.append(firstCopy)
 
-        cycle.sort()
-        cycles.append(cycle)
-        i -= 1
-    
-    currentCycle = []
-    start = round(time.time() * 1000)
+            lastCopy = currentCycle.copy()
+            sum = lastCopy[-1] + lastCopy[-2]
+            lastCopy[-1] = sum
+            del lastCopy[-2]
+            globalCycles.append(lastCopy)
 
-    while round(time.time() * 1000) <= start + msPerGoo:
-        cycleGenerator(cycles[rand(len(cycles) - 1)])
+            middleCopy = currentCycle.copy()
+            sum = middleCopy[math.floor(len(currentCycle) / 2.0)] + middleCopy[math.floor(len(currentCycle) / 2.0) + 1]
+            middleCopy[math.floor(len(currentCycle) / 2.0)] = sum
+            del middleCopy[math.floor(len(currentCycle) / 2.0) + 1]
+            globalCycles.append(middleCopy)
+            
+            return 0
+        elif len(currentCycle == 2):
+            sum = currentCycle[0] + currentCycle[1]
+            currentCycle[0] = sum
+            del currentCycle[1]
+            globalCycles.append(currentCycle)
+    print(len(globalCycles))
 
-    for cycle in cycles:
+    for cycle in globalCycles:
         productOfCycleLengths.append(lcmArray(cycle))
 
-    i = 0
-    while i < len(productOfCycleLengths):
+    for i in range (0, len(productOfCycleLengths),1):
         if productOfCycleLengths[i] > max:
             max = productOfCycleLengths[i]
-            using = cycles[i]
-        i += 1
+            using = globalCycles[i]
 
     return [max, using]
 
@@ -99,22 +99,11 @@ def gcd(a, b):
     else:
         return gcd(b, a % b)
 def rand(max):
+    #can return 0,1,2,...,max - 1, max
     return random.randint(0, max)
 
-def cycleGenerator(cycle):
-    if (len(cycle) < 3):
-        return 0;
-    
-    newCycle = cycle
-    r1 = rand(len(newCycle) - 1)
-    r2 = rand(len(newCycle) - 1)
-    sum = newCycle[r1] + newCycle[r2]
-    newCycle[r1] = sum
-    del newCycle[r2]
-    newCycle.sort()
-    if (newCycle not in cycles):
-        cycles.append(newCycle)
 
+@functools.lru_cache
 def factorial(n):
     return 1 if n == 0 else n * factorial(n - 1)
 
